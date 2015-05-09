64e2f470b2c514b3157bde38ea38ef9525b66bec
Todd Kronenberg <toddkronenberg@gmail.com>
2015-05-08 20:57:28 -0700

    Initial Commit

---
+++ KidMath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KidMath
+//
+//  Created by Todd Kronenberg on 5/8/15.
+//  Copyright (c) 2015 Todd Kronenberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
